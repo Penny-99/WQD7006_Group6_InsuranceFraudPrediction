d8f22ae19ecfdc1769489f0c2f4bdabbc54a3e66
Tan Jia Hui <76894184+Penny-99@users.noreply.github.com>
2024-05-25 13:59:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,9 @@ import streamlit as st
 import pandas as pd
 import pickle as pkl
 
-import os
-st.write("Current directory:", os.getcwd())
-st.write("List of files in current directory:", os.listdir('.'))
-
 # # loading in the model to predict on the data
 # pickle_in = open('rf2.pkl', 'rb')
-# rf2 = pickle.load(open('rf2.pkl', 'rb'))
+# rf2 = pickle.load(open('/mount/src/wqd7006_group6_insurancefraudprediction/rf2.pkl', 'rb'))
 with open('WQD7006_Group6_InsuranceFraudPrediction/rf2.pkl', 'rb') as f:
 	rf2 = pkl.load(f)
 
